1bd54c106f1dbaf85f65b0ac86472dc6886c3993
Ian Zhang <ianzhang1986@gmail.com>
2015-09-26 13:54:53 -0700

    Using polling to check if user content is ready

--- content.js
+++ content.js
@@ -1,3 +1,15 @@
 //document.body.style.background = "yellow";
-var element = document.getElementById("tinymce");
-console.log(element);
+// Polling in 1000 ms to check if the document is ready;
+// TODO: match link script url with target url
+
+var dev = true;
+var pollState = function() {
+  var element = document.getElementById("entinymce_328_ifr");
+  if (element !== null) {
+    element.addEventListener();
+    clearInterval(intervalID);
+    if(dev) { console.log(element); }
+  }
+};
+
+var intervalID = window.setInterval(pollState, 1000);
--- popup.js
+++ popup.js
@@ -1,5 +1,15 @@
 (function() {
   var element = document.getElementById("results");
   katex.render("c = \\pm\\sqrt{a^2 + b^2}", element);
-})()
+
+  var clickHandler = function(e) {
+    chrome.runtime.sendMessage({directive: "popup-click"}, function(response) {
+      this.close();
+    });
+  };
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('click-me').addEventListener('click', clickHandler);
+  });
+})();
 
