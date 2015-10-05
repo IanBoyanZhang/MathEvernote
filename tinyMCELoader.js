06b295c7f3a1ea0b468e62557cd697dbf129128c
Ian Zhang <ianzhang1986@gmail.com>
2015-10-04 23:58:18 -0700

    Cleanup deadCode

--- tinyMCELoader.js
+++ tinyMCELoader.js
@@ -18,24 +18,16 @@
   }, 2000);
 
     var exec = function() {
-  //  console.log(editor.getBody());
-  //  console.log(editor.getContent());
   //  console.log(editor.selection.getRng().textContent);
-    
     editor.execCommand('mceInsertContent', false, "my some text");
 //    editor.execCommand('mceInsertRawHTML', false, "<br clear='none'>");       // create new line
     console.log(editor.selection.getRng().endOffset);
     console.log(editor.selection.getRng());
-    // cash whole line then do the first pass
-    // if event loop comes back detects the current line change or newline
-    // Version 1 detects newline only
-
-   // Set up event listener
+    // Set up event listener
     editorBody.addEventListener('keydown', function(e) {
       if (document.activeElement === editorBody) {
         console.log(e.keyCode);
       }
-//      console.log("Which key is down?",e.keyCode);
     });
   };
 
