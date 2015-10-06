029f581e620953f7943d58f2996964f0b78f97f1
Ian Zhang <ianzhang1986@gmail.com>
2015-10-06 14:33:17 -0700

    MMVP

--- tinyMCELoader.js
+++ tinyMCELoader.js
@@ -4,6 +4,9 @@
 // 3. reach maximum width // or interpret them as same line? just paste the image?
 
 // TODO: checkCurrentCursor // caret position
+// TODO: Escape from back slash
+// TODO: Fix html2canvas
+// TODO: Add mouse action
 (function(exports) {
   'use strict';
   var editorId = "entinymce_328"; 
@@ -18,23 +21,22 @@
     window.clearTimeout(waitEditor);
   }, 2000);
 
-  var render = function(node) {
-    // render the node with picture
-    // 2. take canvas snap shot
-
-    if (node.nodeValue !== null) {
+  var render = function(buildNode, regexResult) {
+    if (buildNode.nodeValue !== null) {
       // create node in tinyMCE fashion
-      var htmlContent = katex.renderToString("c = \\pm\\sqrt{a^2 + b^2}", {displayMode: false});
+      // var htmlContent = katex.renderToString("c = \\pm\\sqrt{a^2 + b^2}", {displayMode: false});
+      console.log(typeof regexResult[1]);
+      var htmlContent = katex.renderToString(regexResult[2], {displayMode: false});
       var elem = document.createElement("div");
       elem.innerHTML = htmlContent;
       // convert elem to canvas
+      editor.selection.setNode(elem);
       html2canvas(elem, {
         onrendered: function(canvas) {
-//          var canvasImg = canvas.toData
           console.log(canvas);
+//          editor.selection.setNode(elem);
         }
       });
-      console.log(editor.selection.setNode(elem));
     }
   };
 
@@ -64,9 +66,9 @@
         mathModeBling.lastIndex++;
       }
 
+      renderCB(textNode, result);
       textVal = wordReplacer(textNode, result);
       textNode.nodeValue = textVal;
-      renderCB(textNode);
     }
   };
 
@@ -77,8 +79,6 @@
         processor(window.getSelection().focusNode, render);
       }
     });
-    // add event listener for mouse actions
-//    editorBody.addEventListener('')
   };
 
 })(this);
